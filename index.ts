c7b86a835c1af9eb74770afdb91d9828b2290854
SinYongJu <ygp007@naver.com>
2023-02-01 00:13:10 +0900

    UPDATE: codes

--- index.ts
+++ index.ts
@@ -1,9 +1,36 @@
 import Builder from "./src/Builder";
 
-const result = Builder.use({
-  name: "luka",
-  age: 29,
-}).build();
+const user = Builder.create({
+  name: "",
+  age: 0,
+})
+  .setName(() => "Smith")
+  .setAge(20)
+  .build();
 
-console.log("hello world");
-console.log(result);
+console.log(user);
+
+class User {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+const user2 = Builder.create<User>(new User("", 0))
+  .setAge(20)
+  .setName("sdsdsd")
+  .build();
+console.log(user2);
+
+const user3 = Builder.create({
+  name: "",
+  age: -1,
+})
+  .setAge(20)
+  .setAge((age) => age + 4)
+  .setName("sdasdasdasdsdsd")
+  .build((user) => new User(user.name, user.age));
+
+console.log(user3);
--- src/Builder.ts
+++ src/Builder.ts
@@ -1,12 +1,53 @@
-export default class Builder<T extends {}> {
-  initial: T;
+interface ICommonBuilder<T> {
+  target: T;
+}
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+
+type IBuilder<T> = Builder<T> & {
+  [k in keyof NonFunctionProperties<T> as `set${Capitalize<string & k>}`]: (
+    value: T[k] | ((setting: T[k]) => T[k])
+  ) => IBuilder<T>;
+};
+export default class Builder<T> implements ICommonBuilder<T> {
+  target: T;
   constructor(initial: T) {
-    this.initial = initial;
+    this.target = initial;
+    return this.toProxyUnit();
   }
-  build() {
-    return this.initial;
+  toProxyUnit() {
+    const proxy = new Proxy(this, {
+      get(target, p, receiver) {
+        if ((p as string).indexOf("set") === 0) {
+          let [_, fst, ...etcs] = String(p).split("set");
+          fst = fst.charAt(0).toLocaleLowerCase() + fst.slice(1);
+          const property = [fst].concat(etcs).join("");
+          return <U extends T[keyof T]>(value: U | ((setting: U) => U)) => {
+            target["target"][property as keyof T] =
+              typeof value === "function"
+                ? (value as Function)(target.target[property as keyof T])
+                : value;
+            return Builder.create(target["target"]);
+          };
+        }
+        return Reflect.get(target, p, receiver);
+      },
+    });
+    return proxy;
   }
-  static use<T extends {}>(initial: T) {
-    return new Builder(initial);
+  build<Fn extends (arg: T) => any>(
+    callback?: Fn
+  ): ReturnType<Fn> extends any ? T : ReturnType<Fn> {
+    if (callback) return callback(this.target);
+    return this.target;
+  }
+  static create<T>(initial: T) {
+    return new Builder(initial) as IBuilder<T>;
   }
 }
+
+const fn = <T>(arg: T) => 2;
+
+fn(2);
